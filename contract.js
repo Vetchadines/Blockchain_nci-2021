475a47bf5d33330683ca3303d62ebd45770f2f9e
Vetchadines <vsvdineshkumar118@gmail.com>
2021-11-30 23:01:08 +0000

    updated files

--- contract.js
+++ contract.js
@@ -1,9 +1,20 @@
 // this is a basic randomly contact interaction file
 const Web3 = require("Web3")
 
-//https://ropsten.infura.io/v3/b0f669f19933420f91796fd7e5959977
-
 
 //instantiate web3
+
+const rpcURL = "https://ropsten.infura.io/v3/b0f669f19933420f91796fd7e5959977";
+
+const web3 = new Web3(rpcURL);
+
+//get the ABI (interface) for our contract
+//connect to our contract on ropsten
+//run some of the methods in our contract (using javascript)
+
+
+
+
+
 console.log("hello world");
 

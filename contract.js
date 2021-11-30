cec2296a8598343526c77755547c3ceb1ac8bd4d
Vetchadines <vsvdineshkumar118@gmail.com>
2021-11-30 22:53:08 +0000

    updated file

--- contract.js
+++ contract.js
@@ -1,5 +1,9 @@
 // this is a basic randomly contact interaction file
 const Web3 = require("Web3")
 
+//https://ropsten.infura.io/v3/b0f669f19933420f91796fd7e5959977
+
+
+//instantiate web3
 console.log("hello world");
 
